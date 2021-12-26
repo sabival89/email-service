ca75b9ea274c65ddb2fb266136f11171ec3702da
Valentine Aduaka <sabival89@gmail.com>
2021-12-26 12:28:57 -0700

    ES-100 Create email DTO and remove unneccessary resource items

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,26 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { CreateEmailDto } from './dto/create-email.dto';
-import { UpdateEmailDto } from './dto/update-email.dto';
+import { SendEmailDto } from './dto/send-email.dto';
 
 @Injectable()
 export class EmailService {
-  create(createEmailDto: CreateEmailDto) {
+  sendEmail(sendEmailDto: SendEmailDto) {
     return 'This action adds a new email';
-  }
-
-  findAll() {
-    return `This action returns all email`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} email`;
-  }
-
-  update(id: number, updateEmailDto: UpdateEmailDto) {
-    return `This action updates a #${id} email`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} email`;
   }
 }
