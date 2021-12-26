42ac8fadfe36ed607ae2eadcc55748b5d339f5c8
Valentine Aduaka <sabival89@gmail.com>
2021-12-26 15:04:11 -0700

    ES-100 Add validation pipe to email controller

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  ValidationPipe,
 } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { SendEmailDto } from './dto/send-email.dto';
@@ -13,12 +14,11 @@ import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Email')
 @Controller('email')
-
 export class EmailController {
   constructor(private readonly emailService: EmailService) {}
 
   @Post()
-  sendEmail(@Body() sendEmailDto: SendEmailDto) {
+  sendEmail(@Body(new ValidationPipe()) sendEmailDto: SendEmailDto) {
     return this.emailService.sendEmail(sendEmailDto);
   }
 }
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { SendEmailDto } from './dto/send-email.dto';
 
 @Injectable()
 export class EmailService {
+  private readonly logger = new Logger(EmailService.name);
+
   sendEmail(sendEmailDto: SendEmailDto) {
+    // this.logger.log('Email process started..');
+
+    // this.logger.error('Cannot process started..');
+
+    console.log(sendEmailDto);
     return 'This action adds a new email';
   }
 }
