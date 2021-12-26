25b05efc309d62c5fe749d3d65e9d336a10df506
Valentine Aduaka <sabival89@gmail.com>
2021-12-26 12:16:29 -0700

    ES-100 Add openapi swagger docs

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,9 @@ async function bootstrap() {
     .setDescription('A Transactional Email Service')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
+  
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
