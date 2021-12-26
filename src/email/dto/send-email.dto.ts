85cfac5e8d2e103906fa8e2bc68ed4dcfdc56669
Valentine Aduaka <sabival89@gmail.com>
2021-12-26 14:39:06 -0700

    ES-100 Add email DTO with Api properties

--- src/email/dto/send-email.dto.ts
+++ src/email/dto/send-email.dto.ts
@@ -1,8 +1,30 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail } from 'class-validator';
+import { IsEmail, MaxLength, MinLength } from 'class-validator';
 
 export class SendEmailDto {
   @ApiProperty({ example: 'johndoe@gmail.com' })
   @IsEmail()
   to: string;
+
+  @ApiProperty({ example: 'John Doe' })
+  @MaxLength(25)
+  @MinLength(3)
+  to_name: string;
+
+  @ApiProperty({ example: 'janedoe@gmail.com' })
+  @IsEmail()
+  from: string;
+
+  @ApiProperty({ example: 'Jane Doe' })
+  @MaxLength(25)
+  @MinLength(3)
+  from_name: string;
+
+  @ApiProperty({ example: 'Payment Reminder' })
+  @MaxLength(30)
+  @MinLength(3)
+  subject: string;
+
+  @ApiProperty({ example: 'Just an email body message...' })
+  body: string;
 }
