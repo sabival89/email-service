f118a3b8a8f7066752c07fbe9ba725851e2ce916
Valentine Aduaka <sabival89@gmail.com>
2021-12-28 17:15:20 -0700

    ES-100 Install nest config library

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,16 +1,38 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { SendEmailDto } from './dto/send-email.dto';
+import * as sendGrid from '@sendgrid/mail';
 
 @Injectable()
 export class EmailService {
   private readonly logger = new Logger(EmailService.name);
 
+  constructor() {
+    sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
+  }
+
   sendEmail(sendEmailDto: SendEmailDto) {
     // this.logger.log('Email process started..');
 
+    const msg = {
+      to: 'test@example.com', // Change to your recipient
+      from: 'test@example.com', // Change to your verified sender
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+
     // this.logger.error('Cannot process started..');
 
-    console.log(sendEmailDto);
+    // sendGrid
+    //   .send(msg)
+    //   .then(() => {
+    //     console.log('Email sent');
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
+
+    console.log(sendEmailDto, process.env.SENDGRID_API_KEY);
     return 'This action adds a new email';
   }
 }
