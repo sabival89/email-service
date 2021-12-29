d2d278be8e38fd3f534ec948c02f62a4bba9e340
Valentine Aduaka <sabival89@gmail.com>
2021-12-29 09:27:49 -0700

    Test mailgun API integration

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,19 +1,59 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { SendEmailDto } from './dto/send-email.dto';
-import * as SendGrid from '@sendgrid/mail';
 import { ConfigService } from '@nestjs/config';
+import * as SendGrid from '@sendgrid/mail';
+import * as Mailgun from 'mailgun-js';
 
 @Injectable()
 export class EmailService {
   private readonly logger = new Logger(EmailService.name);
 
+  // private readonly mailgun: ({}) => void;
+
+  // private mailer = new NodeMailgun();
+
   constructor(private configService: ConfigService) {
     // sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
     SendGrid.setApiKey(this.configService.get<string>('SENDGRID_API_KEY'));
+
+    /* this.mailer.apiKey = this.configService.get<string>('MAILGUN_API_KEY');
+    this.mailer.domain = this.configService.get<string>('MAILGUN_DOMAIN');
+    this.mailer.fromEmail = `vaduaka@${this.configService.get<string>(
+      'MAILGUN_DOMAIN',
+    )}`;
+    this.mailer.fromTitle = 'Just a test email';
+
+    this.mailer.init(); */
   }
 
   sendEmail(sendEmailDto: SendEmailDto) {
     // this.logger.log('Email process started..');
+
+    const mailgun = new Mailgun({
+      apiKey: this.configService.get<string>('MAILGUN_API_KEY'),
+      domain: this.configService.get<string>('MAILGUN_DOMAIN'),
+    });
+
+    const data = {
+      from: `vaduaka@${this.configService.get<string>('MAILGUN_DOMAIN')}`,
+      to: 'valentineaduaka@outlook.com',
+      subject: 'Hello',
+      text: 'Testing some Mailgun awesomness!',
+    };
+
+    /* mailgun.messages().send(data, function (error, body) {
+      console.log(body);
+
+      if (error) {
+        console.log('Error: ', error);
+      }
+    }); */
+
+    mailgun
+      .messages()
+      .send(data)
+      .then((result) => console.log('Done: ', result))
+      .catch((error) => console.error('Error: ', error));
 
     const msg = {
       to: 'valentineaduaka@outlook.com', // Change to your recipient
@@ -23,20 +63,22 @@ export class EmailService {
       html: '<strong>and easy to do anywhere, even with Node.js</strong>',
     };
 
+    /* this.mailer
+      .send('valentineaduaka@outlook.com', 'Hello!', '<h1>hsdf</h1>')
+      .then((result) => console.log('Done', result))
+      .catch((error) => console.error('Error: ', error));
+ */
     // this.logger.error('Cannot process started..');
 
-    SendGrid.send(msg)
+    /* SendGrid.send(msg)
       .then(() => {
         console.log('Email sent');
       })
       .catch((error) => {
         console.error(error);
-      });
+      }); */
 
-    console.log(
-      sendEmailDto,
-      this.configService.get<string>('SENDGRID_API_KEY'),
-    );
+    console.log(this.configService.get<string>('SENDGRID_API_KEY'));
     return 'This action adds a new email';
   }
 }
